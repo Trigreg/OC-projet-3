7ca1f6a2dcba74cffc1e24c028a7f91df725a8ca
Greg <moinegregoire@yahoo.fr>
2018-05-15 11:03:08 +0200

    index réduit à l'appel de fonction sur la bdd

--- index.php
+++ index.php
@@ -1,14 +1,3 @@
-<!DOCTYPE html>
-<html>
-<head>
-	<title>Mon super blog !</title>
-	<meta charset="utf-8">
-</head>
-<body>
-
-<h1>Mon super blog</h1>
-
-<p>Derniers posts du blog :</p>
 
 <?php 
 	try {
@@ -20,19 +9,7 @@
 	}
 
 	$posts=$db->query('SELECT id, title, content, DATE_FORMAT(creation_date, \'%d/%m/%Y à %Hh%imin%ss\') AS creation_date_fr FROM posts ORDER BY id DESC');
-
-	while ($data=$posts->fetch()) {
-	?>
-		<div class="news">
-			<h2><?= $data['title'] . " écrit le " . $data['creation_date_fr']; ?></h2>
-			<p><?= $data['content'] ?></p>
-			<a href="comments.php?post_id=<?= $data['id']; ?>">Commentaires</a>
-		 </div> 
 	
-	<?php
-	}
 
+require('index_view.php');
 ?>
-
-</body>
-</html>
